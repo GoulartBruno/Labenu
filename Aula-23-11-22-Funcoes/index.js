492e3f8b010561889418e196b077c162956b7013
GoulartBruno <brunogoulartdev@gmail.com>
2022-11-23 21:38:54 +0100

    Exercicios dia 23/11/2022

---
+++ Aula-23-11-22-Funcoes/index.js
@@ -0,0 +1,34 @@
+/*
+
+function pratica1Normal(nome) {
+    console.log(`olá ${nome}`)
+
+}
+
+
+const pratica1Arrow = (nome) => {console.log(`ola ${nome}`)}
+
+
+
+
+pratica1Normal("joao")
+pratica1Arrow(" bananinha")
+
+
+pratica1Normal("Fulana")
+pratica1Arrow(" ciclana")
+
+
+
+const pratica1ComReturn = (nome) => {
+    
+    return (`ola ${nome}`)}
+
+    console.log(pratica1ComReturn("teste"))
+
+*/
+
+
+
+
+
