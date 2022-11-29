1b32d4480ce1396d896bc0f47fa2ce79ab670653
GoulartBruno <brunogoulartdev@gmail.com>
2022-11-29 22:52:37 +0100

    exercicios aula objetos

---
+++ Aula-29-11-2022-Objetos/objetos-template/script.js
@@ -0,0 +1,116 @@
+
+/*
+const estudante = {
+    nome: "Bruno",
+    sobrenome: "Goulart",
+    matricula: 25456,
+    notas: [4, 5, 6, 8]
+
+}
+
+console.log(estudante.notas[1])
+
+estudante.modulo="modulo2"
+
+
+
+const novoEstudante = {
+        ... estudante,
+    nome: "Astrodev",
+    notas: [...estudante.notas, 9],
+    modulo: `modulo3`
+
+}
+
+console.log(novoEstudante)
+
+
+const estudantesLabenu = ["Bruno", "Michelle", "Ronaldinho"]
+estudantesLabenu.push(novoEstudante)
+console.log(estudantesLabenu)
+
+
+*/
+
+
+
+
+
+
+
+
+/*
+
+estudante.modulo = " Módulo 1"
+estudante["modulo"] = "Módulo 1"
+console.log(estudante.nome)
+
+console.log("estudante:", estudante)
+console.log("nome:", estudante.nome)
+console.log("segunda nota:", estudante.notas[1])
+console.log("Módulo:", estudante.modulo)
+
+
+const novoEstudante = {
+    ... estudante,
+    nome: "Astrodev",
+    notas: [...estudante.notas, 9],
+    modulo: "modulo 2"
+}
+
+
+novoEstudante.notas.push(9)
+console.table("novo estudante", novoEstudante)
+
+//criar um array com os dois estudantes
+
+const estudantesLabenu = ["Bruno", "Michelle", "Ronaldinho"]
+estudantesLabenu.push(novoEstudante)
+console.log(estudantesLabenu)*/
+
+/*
+
+const cart = {
+    client: "Bruno",
+    paymentMethods: ["cart", "cash", "paypal"],
+    address: "Rua floriano das uvas",
+
+}
+
+cart.shoppings= [
+    {
+    nome: "trousers",
+    preco: 145,
+    quantidade: 4
+},
+{
+    nome: "t-shirt",
+    preco: 42,
+    quantidade: 2
+},
+{
+    nome: "sock",
+    preco: 8,
+    quantidade: 8
+}
+]
+
+console.log(cart)
+
+
+console.log(cart.shoppings[0].quantidade + cart.shoppings[1].quantidade + cart.shoppings[2].quantidade)
+
+
+const giftCart= {
+    ... cart,
+    client: "henrique",
+    paymentMethods:"cart"
+
+}
+
+
+
+console.log(giftCart)
+
+
+*/
