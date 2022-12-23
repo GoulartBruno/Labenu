297ede4c9b85bc69e890ae34eae4e0109cefb609
GoulartBruno <brunogoulartdev@gmail.com>
2022-12-23 19:03:49 +0100

    exercices

--- week-03/lesson-12-06-2022-loop2/lacos-II-exercicios/exercicio2/script.js
+++
@@ -1,8 +0,0 @@
-let tabuada = +prompt(`Digite um número para saber a tabuada.`);
-let resultado = [, , , , , , , , , ,];
-
-for (let i of resultado) {
-  let resultado1 = i * tabuada;
-
-  console.log(resultado1[i]);
-}
